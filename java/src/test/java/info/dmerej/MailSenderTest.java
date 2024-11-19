a5b78cf3ae52c6aeee7200e6ebc4e0b86205dba7
CherifDM <alhassan.deme18@gmail.com>
2024-11-19 15:38:38 +0100

    Fix test DiscountApplierTest

--- java/src/test/java/info/dmerej/MailSenderTest.java
+++ java/src/test/java/info/dmerej/MailSenderTest.java
@@ -1,15 +1,45 @@
 package info.dmerej;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import info.dmerej.mailprovider.SendMailRequest;
+import info.dmerej.mailprovider.SendMailResponse;
 
 public class MailSenderTest {
+
+    User user = new User("Meunier", "meunier@gmail.com");
+    SendMailRequest sendMailRequest;
+    Object requestObject;
+
+    MailSender mailSender = new MailSender(new HttpClient() {
+        @Override
+        public SendMailResponse post(String url, Object request) {
+            sendMailRequest = (SendMailRequest) request;
+            return null;
+        }
+    });
+
+    MailSender failedMailSender = new MailSender(new HttpClient() {
+        @Override
+        public SendMailResponse post(String url, Object request) {
+            requestObject = request;
+            return new SendMailResponse(503, "Error http");
+        }
+    });
+
     @Test
     void should_make_a_valid_http_request() {
-        // TODO: write a test to demonstrate the bug in MailSender.sendV1()
+        mailSender.sendV1(user, "Hello World?");
+        assertEquals("meunier@gmail.com", sendMailRequest.recipient());
+        assertEquals("New notification", sendMailRequest.subject());
+        assertEquals("Hello World?", sendMailRequest.body());
     }
 
     @Test
     void should_retry_when_getting_a_503_error() {
-        // TODO: write a test to demonstrate the bug in MailSender.sendV2()
+        SendMailRequest request = new SendMailRequest("meunier@gmail.com", "New notification", "Hello World?");
+        failedMailSender.sendV2(user, "Hello World?");
+        assertEquals(requestObject, request);
     }
 }
