519df1a80b6a880b6a16079ffb03136bec96cf16
CherifDM <alhassan.deme18@gmail.com>
2024-11-19 14:47:11 +0100

    Fix test SafeCalculatorTest

--- java/src/test/java/info/dmerej/SafeCalculatorTest.java
+++ java/src/test/java/info/dmerej/SafeCalculatorTest.java
@@ -2,9 +2,34 @@ package info.dmerej;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SafeCalculatorTest {
+
+  SafeCalculator unauthorizedSafeCalculator = new SafeCalculator(new Authorizer() {
+    @Override
+    public boolean authorize() {
+      return false;
+    }
+  });
+
+  SafeCalculator authorizedSafeCalculator = new SafeCalculator(new Authorizer() {
+    @Override
+    public boolean authorize() {
+      return true;
+    }
+  });
   @Test
   void should_not_throw_when_authorized() {
-    // TODO: write a test to demonstrate the bug in SafeCalculator.add()
+    assertDoesNotThrow(() -> {
+      authorizedSafeCalculator.add(1,2);
+    });
+  }
+
+  @Test
+  void should_throw_when_not_authorized() {
+    assertThrows(UnauthorizedAccessException.class, () -> {
+      unauthorizedSafeCalculator.add(1,2);
+    });
   }
 }
