22bc7ca14fb7d67a16091a107d4089a022f0dca4
CherifDM <alhassan.deme18@gmail.com>
2024-11-19 15:47:06 +0100

    Fix test SafeCalculator with mockito

--- java/src/test/java/info/dmerej/SafeCalculatorTest.java
+++ java/src/test/java/info/dmerej/SafeCalculatorTest.java
@@ -1,10 +1,15 @@
 package info.dmerej;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 public class SafeCalculatorTest {
+
+  Authorizer mockAuthorizer = Mockito.mock(Authorizer.class);
+
 
   SafeCalculator unauthorizedSafeCalculator = new SafeCalculator(new Authorizer() {
     @Override
@@ -30,6 +35,26 @@ public class SafeCalculatorTest {
   void should_throw_when_not_authorized() {
     assertThrows(UnauthorizedAccessException.class, () -> {
       unauthorizedSafeCalculator.add(1,2);
+    });
+  }
+
+  @Test
+  void should_not_throw_when_authorized_v2() {
+    when(mockAuthorizer.authorize()).thenReturn(true);
+    SafeCalculator safeCalculator = new SafeCalculator(mockAuthorizer);
+
+    assertDoesNotThrow(() -> {
+      safeCalculator.add(1,2);
+    });
+  }
+
+  @Test
+  void should_throw_when_not_authorized_v2() {
+    when(mockAuthorizer.authorize()).thenReturn(false);
+    SafeCalculator safeCalculator = new SafeCalculator(mockAuthorizer);
+
+    assertThrows(UnauthorizedAccessException.class, () -> {
+      safeCalculator.add(1,2);
     });
   }
 }
